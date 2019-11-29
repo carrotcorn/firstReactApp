236adb4c46128499eb94d8c7321471c1622c6ffe
carrotcorn <47840684+carrotcorn@users.noreply.github.com>
2019-11-29 14:12:24 -0800

    added an addData. js
    add js added, and changed appJSX

---
+++ src/addData.js
@@ -0,0 +1,32 @@
+const addData = [
+  {
+    id: 1,
+    tag: "eric",
+    name: "Eric Andre",
+    imgUrl: "https://i.ytimg.com/vi/8OWsHkIMTsg/maxresdefault.jpg",
+    phone: "219 573 8383"
+  },
+  {
+    id: 2,
+    name: "Pablo Escobar",
+    imgUrl:
+      "https://cdn.britannica.com/15/188715-050-94EA4753/Pablo-Escobar-1991.jpg",
+    phone: "+3 73 29 92 18"
+  },
+  {
+    id: 3,
+    name: "Hank Hill",
+    imgUrl: "https://i.redd.it/j2jdb56u6n9z.jpg",
+    phone: "504 983 8373"
+  },
+  {
+    id: 4,
+    tag: "joe",
+    name: "Joe Rogan",
+    imgUrl:
+      "https://media1.tenor.com/images/39227133f978ee222942a4566ee9971c/tenor.gif?itemid=12472661",
+    phone: "219 098 3827"
+  }
+];
+
+export default addData;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,25 @@
 import React from "react";
 import Header from "./Header";
 import Ad from "./Ad";
+import addData from "../addData";
+
+function createAdd(ad) {
+  return (
+    <Ad
+      tag={ad.tag}
+      key={ad.id}
+      name={ad.name}
+      img={ad.imgUrl}
+      phone={ad.phone}
+    />
+  );
+}
 
 function App() {
   return (
     <div className="wrapper">
       <Header />
-      <Ad
-        tag="eric"
-        name="Eric Andre"
-        img="https://i.ytimg.com/vi/8OWsHkIMTsg/maxresdefault.jpg"
-        phone="219 573 8383"
-      />
-      <Ad
-        name="Pablo Escobar"
-        img="https://cdn.britannica.com/15/188715-050-94EA4753/Pablo-Escobar-1991.jpg"
-        phone="+3 73 29 92 18"
-      />
-      <Ad
-        name="Hank Hill"
-        img="https://i.redd.it/j2jdb56u6n9z.jpg"
-        phone="504 983 8373"
-      />
-      <Ad
-        tag="joe"
-        name="Joe Rogan"
-        img="https://media1.tenor.com/images/39227133f978ee222942a4566ee9971c/tenor.gif?itemid=12472661"
-        phone="219 098 3827"
-      />
+      {addData.map(createAdd)}
     </div>
   );
 }
